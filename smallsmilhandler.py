b69ad612029718e8ee746c3c7aa28d8d4f4b6f32
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-10-03 10:59:50 +0200

    Empezando el programa

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__ (self):
+        self.root-layout = " "
+        
+if __name__ == " __main__":
+
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+           
+        
+
 
