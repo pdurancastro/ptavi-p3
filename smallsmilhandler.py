17d0f4b7ec535f3f002f7e9569f397448e38335f
pdurancastro <patryharuno@gmail.com>
2016-10-07 00:07:51 +0200

    Root-layout implementado con exito

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,48 +7,48 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__ (self):
-    
-        self.width = "0"
-        self.height = "0" 
+
+        self.width = ""
+        self.height = ""
         self.background_color = ""
         self.id = ""
-        self.top = "0"
-        self.bottom = "0"
-        self.left = "0"
-        self.right = "0"
-    
+        self.top = ""
+        self.bottom = ""
+        self.left = ""
+        self.right = ""
+
+        self.tags=[]
+
     def startElement(self, name, attrs):
-    
+
         if name == 'root-layout':
             self.width = attrs.get('width',"")
             self.height = attrs.get('height',"")
-            self.background_color = attrs.get('background_color',"")
-            
-        if name == 'region':
-            self.id = attrs.get('id',"")
-            self.top = attrs.get('top',"")
-            self.bottom = attrs.get('bottom',"")
-            self.left = attrs.get('left',"")
-            self.right = attrs.get('right',"")    
-            
-            
-            
-        
-    
+            self.background_color = attrs.get('background-color',"")
+            att={'width': self.width, 'height': self.height,
+                'background_color': self.background_color}
+            roottag={'root-layout':att}
+            self.tags.append(roottag)
+            #print(self.tags)
 
-    
+
+
+
+
+
+
+
+
+
+
     #def get_tags(self):
         #return self.misdatos
-    
-        
-        
+
+
+
 if __name__ == "__main__":
 
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-           
-        
-
-
