9e88d643065ae4aac29316bccf8ddbee04aca417
pdurancastro <patryharuno@gmail.com>
2016-10-07 01:00:41 +0200

    PEP8 PASADO

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,9 +4,10 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
 
         self.width = ""
         self.height = ""
@@ -20,57 +21,54 @@ class SmallSMILHandler(ContentHandler):
         self.region = ""
         self.begin = ""
         self.dur = ""
-
-        self.tags=[]
+        self.tags = []
 
     def startElement(self, name, attrs):
 
         if name == 'root-layout':
-            self.width = attrs.get('width',"")
-            self.height = attrs.get('height',"")
-            self.background_color = attrs.get('background-color',"")
-            att={'width': self.width, 'height': self.height,
-                'background_color': self.background_color}
-            roottag={'root-layout': att}
+            self.width = attrs.get('width', "")
+            self.height = attrs.get('height', "")
+            self.background_color = attrs.get('background-color', "")
+            att = {'width': self.width, 'height': self.height,
+                   'background_color': self.background_color}
+            roottag = {'root-layout': att}
             self.tags.append(roottag)
-            #print(self.tags)
 
         elif name == 'region':
-            self.id = attrs.get('id',"")
-            self.top = attrs.get('top',"")
-            self.bottom = attrs.get('bottom',"")
-            self.left = attrs.get('left',"")
-            self.right = attrs.get('rigth',"")
-            att={'id': self.id, 'top': self.top, 'bottom': self.bottom,
-                'left': self.left, 'right': self.right}
-            regiontag={'region': att}
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('rigth', "")
+            att = {'id': self.id, 'top': self.top, 'bottom': self.bottom,
+                   'left': self.left, 'right': self.right}
+            regiontag = {'region': att}
             self.tags.append(regiontag)
 
         elif name == 'img':
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
-            att={'src': self.src, 'region': self.region, 'begin': self.begin,
-                'dur': self.dur}
-            imgtag={'img': att}
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            att = {'src': self.src, 'region': self.region, 'begin': self.begin,
+                   'dur': self.dur}
+            imgtag = {'img': att}
             self.tags.append(imgtag)
 
         elif name == 'audio':
-            self.src = attrs.get('src',"")
-            self.begin = attrs.get('begin',"")
-            self.dur = attrs.get('dur',"")
-            att={'src': self.src, 'begin': self.begin, 'dur': self.dur}
-            audiotag={'audio':att}
+            self.src = attrs.get('src', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            att = {'src': self.src, 'begin': self.begin, 'dur': self.dur}
+            audiotag = {'audio': att}
             self.tags.append(audiotag)
 
         elif name == 'textstream':
-            self.src = attrs.get('src',"")
-            self.region = attrs.get('region',"")
-            att={'src': self.src, 'region': self.region}
-            texttag={'textstream':att}
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            att = {'src': self.src, 'region': self.region}
+            texttag = {'textstream': att}
             self.tags.append(texttag)
-
 
     def get_tags(self):
         return self.tags
@@ -82,5 +80,5 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-    mytags=cHandler.get_tags()
+    mytags = cHandler.get_tags()
     print(mytags)
