fca5d1706c4beae9bb075db7b6efaedd3316b8d8
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-10-10 12:49:14 +0200

    Punto4 terminado rev JSON

--- karaoke.py
+++ karaoke.py
@@ -6,6 +6,7 @@ from xml.sax.handler import ContentHandler
 
 import sys
 import smallsmilhandler
+import json
 
 
 
@@ -21,10 +22,19 @@ if __name__ == "__main__":
     parser.setContentHandler(cHandler)
     parser.parse(open(ficherosys))
     mytags = cHandler.get_tags() 
-    print(mytags)
     
     
-    print(mytags[0])
+    for Lista in mytags:
+        for tag in Lista:
+            newatt=""      
+            for att, value in Lista[tag].items():
+                newatt= newatt + '\t' + att + "=" + value + '\t'
+        print (tag + newatt + '\n')
+        
+    json.dumps(mytags, open('karaoke.json'))
+    
+            
+    
     
     
 
