7b1b29dfd34c1906545a0c4ea361be93283c0bfb
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-10-10 11:07:35 +0200

    4.1 Realizado

--- karaoke.py
+++ karaoke.py
@@ -5,4 +5,28 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 import sys
-import SmallSMILHandler
+import smallsmilhandler
+
+
+
+if __name__ == "__main__":
+
+    try:
+        ficherosys = sys.argv[1]
+    except:
+        sys.exit("Usage: python3 karaoke.py file.smil.")
+            
+    parser = make_parser()
+    cHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open(ficherosys))
+    mytags = cHandler.get_tags() 
+    print(mytags)
+    
+    
+    print(mytags[0])
+    
+    
+
+
+
