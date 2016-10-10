52b7dc4fb17454f1021e57edec4d34f81c47d9af
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-10-10 17:07:46 +0200

    PEP8 pasado corregido error en to-json

--- karaoke.py
+++ karaoke.py
@@ -9,47 +9,47 @@ import smallsmilhandler
 import json
 import urllib.request
 
+try:
+    ficherosys = sys.argv[1]
+except:
+    sys.exit("Usage: python3 karaoke.py file.smil.")
 
+
+class KaraokeLocal():
+
+    def __init__(self, ficherosys):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(ficherosys))
+        self.mytags = cHandler.get_tags()
+
+    def __str__(self):
+        for Lista in self.mytags:
+            for tag in Lista:
+                newatt = " "
+                for att, value in Lista[tag].items():
+                    newatt = newatt + '\t' + att + "=" + value + '\t'
+            print(tag + newatt + '\n')
+
+    def to_json(self, var_json, nuevo_nombre=""):
+        var_json = sys.argv[1][:-4]+"json"
+        json.dump(self.mytags, open(var_json, 'w'))
+
+    def do_local(self):
+        for Lista in self.mytags:
+            for tag in Lista:
+                for att, value in Lista[tag].items():
+                    if 'src' in att:
+                        if value[:4] == "http":
+                            urllib.request.urlretrieve(value,
+                                                       value.split('/')[-1])
 
 if __name__ == "__main__":
 
-    try:
-        ficherosys = sys.argv[1]
-    except:
-        sys.exit("Usage: python3 karaoke.py file.smil.")
-            
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(ficherosys))
-    mytags = cHandler.get_tags() 
-    
-    
-    for Lista in mytags:
-        for tag in Lista:
-            newatt=""      
-            for att, value in Lista[tag].items():
-                newatt= newatt + '\t' + att + "=" + value + '\t'
-        print (tag + newatt + '\n')
-        
-   
-    #Pasar mi karaoke SMIL a JSON
-    var_json = sys.argv[1][:-4]+"json"
-    json.dump(mytags,open (var_json, 'w'))
-    
-    
-    
-    for Lista in mytags:
-        for tag in Lista:
-            for att, value in Lista[tag].items():
-                if 'src' in att:
-                    if value[:4] == "http":                                                               
-                        urllib.request.urlretrieve(value,value.split('/')[-1])
-                        
-                  
-                        
-    
-    
-
-
-
+        karaokelocal1 = KaraokeLocal(ficherosys)
+        karaokelocal1.__str__()
+        karaokelocal1.to_json(ficherosys)
+        karaokelocal1.do_local()
+        karaokelocal1.to_json(ficherosys, "local.json")
+        karaokelocal1.__str__()
